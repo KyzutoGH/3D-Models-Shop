07708d20411df624113b0d907a3c1a4a602de047
ALcitya <ichwanalif55@gmail.com>
2024-12-08 15:54:45 +0700

    menambahkan gambar

--- pages/index.js
+++ pages/index.js
@@ -3,36 +3,68 @@ import Layout from '../components/components';
 const products = [
   {
     id: 1,
-    name: 'Earthen Bottle',
+    name: 'Porsche',
     href: '#',
-    price: '$48',
-    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-01.jpg',
-    imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    price: '127000',
+    imageSrc: '/img/car.jpg',
+    imageAlt: 'kjfsfkgjk',
   },
   {
     id: 2,
-    name: 'Nomad Tumbler',
+    name: 'Helm Robot',
     href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-02.jpg',
+    price: '190000',
+    imageSrc: '/img/helm_2.png',
     imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
   },
   {
     id: 3,
-    name: 'Focus Paper Refill',
+    name: 'Storage Car Modern',
     href: '#',
-    price: '$89',
-    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-03.jpg',
+    price: '47500',
+    imageSrc: '/img/gudang.jpeg',
     imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
   },
   {
     id: 4,
-    name: 'Machined Mechanical Pencil',
+    name: 'Helm Robot Natural Colours',
     href: '#',
-    price: '$35',
-    imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-04.jpg',
+    price: '190000',
+    imageSrc: '/img/helm.png',
     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
   },
+  {
+    id: 5,
+    name: 'Shark',
+    href: '#',
+    price: '270000',
+    imageSrc: '/img/hiu.png',
+    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+  },
+  {
+    id: 6,
+    name: 'Shark Teeth',
+    href: '#',
+    price: '240000',
+    imageSrc: '/img/shark_meme.png',
+    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+  },
+  {
+    id:7,
+    name: 'Lego Person',
+    href: '#',
+    price: '300000',
+    imageSrc: '/img/lego_person.png',
+    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+  },
+  {
+    id:8,
+    name: 'Lego Width Gestur',
+    href: '#',
+    price: '310000',
+    imageSrc: '/img/lego_postur.png',
+    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+  }
   // More products...
 ];
 
@@ -44,17 +76,24 @@ const HomePage=() => {
         <h2 className="sr-only">Products</h2>
 
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
-              <img
-                alt={product.imageAlt}
-                src={product.imageSrc}
-                className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]"
-              />
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-            </a>
-          ))}
+          {products
+            .filter(
+              (product, index, self) =>
+                index === self.findIndex((p) => p.id === product.id) // Menghapus duplikat
+            )
+            .map((product) => (
+              <a key={product.id} href={product.href} className="group">
+                <img
+                  alt={product.imageAlt}
+                  src={product.imageSrc}
+                  className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]"
+                />
+                <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">
+                  Rp {product.price},00
+                </p>
+              </a>
+            ))}
         </div>
       </div>
     </div>
